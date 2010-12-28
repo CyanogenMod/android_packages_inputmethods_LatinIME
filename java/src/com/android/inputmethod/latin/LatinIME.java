c64316282efca9e8566873adef69cef14452f0b4
robert wilson <hotaru@thinkindifferent.net>
2010-12-28 07:08:23 -0500

    fix replacing words
    Change-Id: Icf1e3abe44e7b5ba4af51a5f21167858a9409b5d

--- java/src/com/android/inputmethod/latin/LatinIME.java
+++ java/src/com/android/inputmethod/latin/LatinIME.java
@@ -1993,6 +1993,9 @@ public class LatinIME extends InputMethodService
         InputConnection ic = getCurrentInputConnection();
         if (ic != null) {
             rememberReplacedWord(suggestion);
+            if (correcting) {
+                EditingUtil.deleteWordAtCursor(ic, mWordSeparators);
+            }
             ic.commitText(suggestion, 1);
         }
         saveWordInHistory(suggestion);
