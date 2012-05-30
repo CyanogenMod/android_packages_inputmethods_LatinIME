9ab0bdc1743092983829cf70def03774c8b376ed
Konsta <konsta09@gmail.com>
2012-05-30 15:45:05 +0300

    LatinIME: Option to reverse volume key logic on cursor control
    Adds preference box to reverse volume key logic on cursor control.
    Adds preference category for volume key cursor controls.
    
    Change-Id: Ic376859ee7efb7867d91450567d04938bbb6da7e

--- java/src/com/android/inputmethod/latin/LatinIME.java
+++ java/src/com/android/inputmethod/latin/LatinIME.java
@@ -1114,12 +1114,17 @@ public class LatinIME extends InputMethodServiceCompatWrapper implements Keyboar
             }
             break;
         case KeyEvent.KEYCODE_VOLUME_UP:
+            if (mKeyboardSwitcher.isInputViewShown() && mSettingsValues.mEnableVolumeCursor) {
+                sendDownUpKeyEvents((mSettingsValues.mEnableVolumeCursorReverse ? KeyEvent.KEYCODE_DPAD_LEFT
+                        : KeyEvent.KEYCODE_DPAD_RIGHT));
+                return true;
+                }
         case KeyEvent.KEYCODE_VOLUME_DOWN:
             if (mKeyboardSwitcher.isInputViewShown() && mSettingsValues.mEnableVolumeCursor) {
-                sendDownUpKeyEvents((keyCode == KeyEvent.KEYCODE_VOLUME_UP ? KeyEvent.KEYCODE_DPAD_RIGHT
+                sendDownUpKeyEvents((mSettingsValues.mEnableVolumeCursorReverse ? KeyEvent.KEYCODE_DPAD_RIGHT
                         : KeyEvent.KEYCODE_DPAD_LEFT));
                 return true;
-            }
+                }
             break;
         }
         return super.onKeyDown(keyCode, event);
