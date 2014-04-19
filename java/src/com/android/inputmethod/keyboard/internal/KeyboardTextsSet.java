f5dbfc96fe2b1e5e9d3f45d5e4dbe93cd3cfc70d
elektroschmock <elektroschmock78@googlemail.com>
2014-04-19 10:25:08 +0200

    LatinIME: Add @ character to DE keyboard's more keys
    Change-Id: I6a85496e579170a67352160277bfb43125b1e892

--- java/src/com/android/inputmethod/keyboard/internal/KeyboardTextsSet.java
+++ java/src/com/android/inputmethod/keyboard/internal/KeyboardTextsSet.java
@@ -951,6 +951,7 @@ public final class KeyboardTextsSet {
     /* Language de: German */
     private static final String[] LANGUAGE_de = {
         // U+00E4: "ä" LATIN SMALL LETTER A WITH DIAERESIS
+        // U+0040: "@" COMMERCIAL AT
         // U+00E2: "â" LATIN SMALL LETTER A WITH CIRCUMFLEX
         // U+00E0: "à" LATIN SMALL LETTER A WITH GRAVE
         // U+00E1: "á" LATIN SMALL LETTER A WITH ACUTE
@@ -958,7 +959,7 @@ public final class KeyboardTextsSet {
         // U+00E3: "ã" LATIN SMALL LETTER A WITH TILDE
         // U+00E5: "å" LATIN SMALL LETTER A WITH RING ABOVE
         // U+0101: "ā" LATIN SMALL LETTER A WITH MACRON
-        /* 0 */ "\u00E4,\u00E2,\u00E0,\u00E1,\u00E6,\u00E3,\u00E5,\u0101",
+        /* 0 */ "\u00E4,\u0040,\u00E2,\u00E0,\u00E1,\u00E6,\u00E3,\u00E5,\u0101",
         // U+00E9: "é" LATIN SMALL LETTER E WITH ACUTE
         // U+00E8: "è" LATIN SMALL LETTER E WITH GRAVE
         // U+00EA: "ê" LATIN SMALL LETTER E WITH CIRCUMFLEX
